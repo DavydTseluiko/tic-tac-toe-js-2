f591b37ae096c68689efb0e065025268d0aaddbe
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-08-11 15:04:23 -0500

    Make possible to play game with no rules

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-  const board = new Array(9);
+  const board = new Array(9).fill("-");
 
   return { board };
 })();
@@ -14,4 +14,61 @@ const game = (function () {
 
   const firstToGo = firstPlayer.mark === "X" ? firstPlayer : secondPlayer;
   const secondToGo = firstPlayer.mark === "X" ? secondPlayer : firstPlayer;
+
+  let round = 0;
+
+  function displayBoard() {
+    console.clear();
+    for (let i = 0; i < 3; i++) {
+      console.log(gameBoard.board.slice(i * 3, i * 3 + 3).join(" | "));
+      if (i < 2) {
+        console.log("---------");
+      }
+    }
+  }
+
+  function playersChoice(player) {
+    let choice;
+    do {
+      choice = +prompt(`${player.name}, it's your time to go`) - 1;
+    } while (gameBoard.board[choice] !== "-");
+
+    gameBoard.board[choice] = player.mark;
+  }
+
+  function botsChoice(player) {
+    let choice;
+    do {
+      choice = Math.floor(Math.random() * 9);
+    } while (gameBoard.board[choice] !== "-");
+
+    gameBoard.board[choice] = player.mark;
+  }
+
+  function makeChoice(player) {
+    if (player.name !== "Bot") {
+      playersChoice(player);
+    } else {
+      botsChoice(player);
+    }
+    displayBoard();
+  }
+
+  function playRound() {
+    displayBoard();
+    if (round % 2 === 0) {
+      makeChoice(firstToGo);
+    } else {
+      makeChoice(secondToGo);
+    }
+    round++;
+  }
+
+  function playGame() {
+    while (round < 9) {
+      playRound();
+    }
+  }
+
+  playGame();
 })();
